6707e306335fba6efbd1402dc366fd466f5d84d0
Ezaú Lira <149651629+EzauLira@users.noreply.github.com>
2023-12-09 12:26:35 -0300

    Update desafioSurpresa.js

--- desafioSurpresa.js
+++ desafioSurpresa.js
@@ -1,12 +1,12 @@
         //Informações dos Pokemons:
         const statusPokemons = [
-            ["Nome: POOCHYENA, " + "level: 2, " + "Sexo: Masculino, " + "Pontos de vida: 13" + "Pokemon Cadastrado com sucesso!"],
-            ["Nome: ZIGZAGOON, " + "level: 2, " + "Sexo: Feminino, " + "Pontos de vida: 13" + "Pokemon Cadastrado com sucesso!"],
-            ["Nome: DRAGONITE, " + "level: 5, " + "Sexo: Masculino, " + "Pontos de vida: 25" + "Pokemon Cadastrado com sucesso!"],
-            ["Nome: DRAGONITE2, " + "level: 5, " + "Sexo: Feminino, " + "Pontos de vida: 24" + "Pokemon Cadastrado com sucesso!"],
-            ["Nome: DAGONITE3, " + "level: 5, " + "Sexo: Feminino, " + "Pontos de vida: 24" + "Pokemon Cadastrado com sucesso!"],
-            ["Nome: POOCHYENA2, " + "level: 3, " + "Sexo: Feminino, " + "Pontos de vida: 15" + "Pokemon Cadastrado com sucesso!"],
-            ["Nome: WURMPLE, " + "level: 2, " + "Sexo: Masculino, " + "Pontos de vida: 14" + "Pokemon Cadastrado com sucesso!"],
+            ["Nome: POOCHYENA, " + "level: 2, " + "Sexo: Masculino, " + "Pontos de vida: 13" + "Pokémon Cadastrado com sucesso!"],
+            ["Nome: ZIGZAGOON, " + "level: 2, " + "Sexo: Feminino, " + "Pontos de vida: 13" + "Pokémon Cadastrado com sucesso!"],
+            ["Nome: DRAGONITE, " + "level: 5, " + "Sexo: Masculino, " + "Pontos de vida: 25" + "Pokémon Cadastrado com sucesso!"],
+            ["Nome: DRAGONITE2, " + "level: 5, " + "Sexo: Feminino, " + "Pontos de vida: 24" + "Pokémon Cadastrado com sucesso!"],
+            ["Nome: DAGONITE3, " + "level: 5, " + "Sexo: Feminino, " + "Pontos de vida: 24" + "Pokémon Cadastrado com sucesso!"],
+            ["Nome: POOCHYENA2, " + "level: 3, " + "Sexo: Feminino, " + "Pontos de vida: 15" + "Pokémon Cadastrado com sucesso!"],
+            ["Nome: WURMPLE, " + "level: 2, " + "Sexo: Masculino, " + "Pontos de vida: 14" + "Pokémon Cadastrado com sucesso!"],
 ]
         //Saída no console
         console.log(statusPokemons[0])
